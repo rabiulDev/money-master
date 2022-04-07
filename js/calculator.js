eb4be9baffea18315b53be1e888f439e77d066f4
rabiulDev <rabiul.dev@gmail.comm>
2022-04-07 10:12:23 +0600

    calculate function implemented

--- js/calculator.js
+++ js/calculator.js
@@ -1,23 +1,61 @@
 function getInputNumber(id) {
     const element = document.getElementById(id).value;
     const valueAsNumber = parseFloat(element);
-    if (isNaN(valueAsNumber) || valueAsNumber < 0) {
-        const errMassage = document.getElementById("err-message")
-        errMassage.innerText = "Please give all the value as a positive Number.";
-    }
-    else {
-        return valueAsNumber;
-    }
+    return valueAsNumber;
+
 
 }
 
+function setValue(id) {
+    const element = document.getElementById(id);
+    element.value = "";
+}
 
 // HANDLE CALCULATE BUTTON 
 
 document.getElementById('calculate').addEventListener('click',
     function () {
-        const incomeMoney = getInputNumber("income-field")
-        console.log(incomeMoney)
+        const incomeMoney = getInputNumber("income-field");
+
+        // EXPENSE MONEY 
+        const foodMoney = getInputNumber("food-field");
+        const rentMoney = getInputNumber("rent-field");
+        const clothMoney = getInputNumber("cloth-field")
+
+        const allExpenses = foodMoney + rentMoney + clothMoney;
+
+        if (isNaN(incomeMoney) || isNaN(allExpenses) || incomeMoney < 0 || allExpenses < 0) {
+            document.getElementById("err-message").innerText = "Please give all the number as a positive value"
+            // change input field value 
+            setValue("income-field");
+            setValue("food-field");
+            setValue("rent-field");
+            setValue("cloth-field");
+            document.getElementById("total-expenses").innerText = "00"
+            document.getElementById("balance").innerText = "00"
+        }
+        else if (incomeMoney < allExpenses) {
+            document.getElementById("err-message").innerText = "You haven't enough money "
+            document.getElementById("total-expenses").innerText = "00"
+            document.getElementById("balance").innerText = "00"
+        }
+        else {
+            const totalExpense = allExpenses;
+            document.getElementById("total-expenses").innerText = totalExpense;
+            const balance = incomeMoney - totalExpense;
+            document.getElementById("balance").innerText = balance;
+
+            // change input field value 
+            document.getElementById("err-message").innerText = ""
+            setValue("income-field");
+            setValue("food-field");
+            setValue("rent-field");
+            setValue("cloth-field");
+        }
+
+
+
+
 
     }
 )
